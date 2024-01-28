c77194d7388360da514a2142ae0d71004b442116
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2024-01-28 15:43:26 +0200

    slice

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::slice;
 
+fn main() {
+    let address = 0x01234usize;
+    let r = address as *mut i32;
+
+    let value: &[i32] = unsafe {slice::from_raw_parts_mut(r, 10000)};
+}
+
+
+/*
 
 fn main() {
     let mut v = vec![1, 2,3,4,5,6];
@@ -10,3 +20,4 @@ fn main() {
     assert_eq!(a, &mut [1, 2, 3]);
     assert_eq!(b, &mut [1, 2, 3]);
 }
+*/
