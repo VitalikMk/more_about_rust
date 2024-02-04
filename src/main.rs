620fd8c881dd1f7b704f6b7d55d976138d8a28fa
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2024-02-04 11:42:56 +0200

    add

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
-use std::slice;
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Point {x: 1, y: 0} + Point {x: 2, y: 3},
+        Point {x: 3, y: 3}
+    );
+}
+
+
+
+
+
+/*use std::slice;
 
 fn main() {
     let address = 0x01234usize;
@@ -6,7 +36,7 @@ fn main() {
 
     let value: &[i32] = unsafe {slice::from_raw_parts_mut(r, 10000)};
 }
-
+*/
 
 /*
 
