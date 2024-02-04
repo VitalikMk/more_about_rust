ad389c712c44013c5a9430f4c512d651cae992b2
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2024-02-04 13:14:57 +0200

    animal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-use std::ops::Add;
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
+fn main(){
+    println!("A baby dog is called a {}", Dog::baby_name());
+}
+
+/*use std::ops::Add;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
@@ -22,7 +44,7 @@ fn main() {
         Point {x: 1, y: 0} + Point {x: 2, y: 3},
         Point {x: 3, y: 3}
     );
-}
+}*/
 
 
 
